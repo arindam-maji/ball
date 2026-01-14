4f88101312367a5fa9a9f614524509a86ba68f33
Arindam Maji <118745079+arindam-maji@users.noreply.github.com>
2026-01-14 14:15:59 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,9 +1,12 @@
 import coremltools as ct
 
-mlmodel = ct.converters.onnx.convert(
+print("coremltools version:", ct.__version__)
+
+mlmodel = ct.converters.convert(
     model="tracknet_ball_03.onnx",
+    source="onnx",
     minimum_deployment_target=ct.target.iOS15
 )
 
 mlmodel.save("tracknet_ball_03.mlmodel")
-print("✅ CoreML model generated")
+print("✅ Conversion successful")
